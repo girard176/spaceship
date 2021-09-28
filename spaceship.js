1a87af46756e6d1f520efc60221c4ea7f9f033b2
Andre <girard176@live.com>
2021-09-28 14:16:25 -0700

    Updated updateShipList with data.results

--- spaceship.js
+++ spaceship.js
@@ -6,7 +6,8 @@
 
 // variables
 //const shipListUrl = "https://dog.ceo/api/breeds/list/all";
-const shipListUrl = "https://ll.thespacedevs.com/2.2.0/config/spacecraft/";
+const shipListUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/";
+//const shipListUrl = "https://ll.thespacedevs.com/2.2.0/config/spacecraft/?name=&manufacturer=&in_use=true&human_rated=";
 const shipList = document.getElementById("ship-list"); 
 
 // when the page load
@@ -21,7 +22,8 @@ async function getShipList(){
 function updateShipList(){
     getShipList().then(function(data){
             //get each ship name
-            for(element in data.message){
+            console.log(data);
+            for(element in data.results){
                 // append to the select list
                 let option = createOption(element);
                 shipList.appendChild(option);
@@ -31,6 +33,7 @@ function updateShipList(){
 }
 
 function createOption(text){
+    //let option = document.createElement("option");
     let option = document.createElement("option");
     option.textContent = text;
     return option;
