d26872a086a2480a97b9fbb00b34fa15dee767f6
Andre <girard176@live.com>
2021-09-27 18:22:51 -0700

    Updated js

--- spaceship.js
+++ spaceship.js
@@ -8,17 +8,28 @@ const shipListUrl = "https://ll.thespacedevs.com/2.2.0/config/spacecraft/";
 const shipList = document.getElementByID("ship-list"); 
 
 // when the page load
-window.addEventListener("load", );
+window.addEventListener("load", updateShipList);
 
 // retreive the list of spaceships
-function getShipList(){
-    return fetch(shipListUrl);
+async function getShipList(){
+    return fetch(shipListUrl).then(response => response.json());
 }
 
 // add spaceships to drop down list
 function updateShipList(){
-    getShipList().then(function(data)
-        //get ship name
-        //append to the select list
-    )
+    getShipList().then(function(data){
+        //get each ship name
+        for(element in data.message){
+            // append to the select list
+            let option = createOption(element);
+            shipList.appendChild(option);
+        }
+    }
+    );
+}
+
+function createOption(text){
+    let option = document.createElement("option");
+    option.textContent = text;
+    return option;
 }
