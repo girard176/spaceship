e5dbfba2854b1ac8ecfaa9b68b19f2e7a9bed880
Andre <girard176@live.com>
2021-09-29 15:15:55 -0700

    Added ship data function

--- spaceship.js
+++ spaceship.js
@@ -1,12 +1,16 @@
 /*
     Author: Andre
     JavaScript for the spaceship website
-    API ref: https://thespacedevs.com/llapi
+    API ref: https://lldev.thespacedevs.com/2.2.0/config/spacecraft/
 */
 
 // variables
-const shipListUrl = "https://ll.thespacedevs.com/2.2.0/config/spacecraft/?name=&manufacturer=&in_use=true&human_rated=";
-const shipList = document.getElementById("ship-list"); 
+const shipListUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/?in_use=true&human_rated=true&order=name";
+const shipList = document.getElementById("ship-list");
+const button = document.getElementById("getShip");
+const shipID = 10;
+const shipUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/10/"
+const shipImage = document.getElementById("ship-image")
 
 // when the page load
 window.addEventListener("load", updateShipList);
@@ -19,7 +23,9 @@ async function getShipList(){
 // add spaceships to drop down list
 function updateShipList(){
     getShipList().then(function(data){
-            //get each ship name
+            let option = createOption("Select a spaceship");
+            shipList.appendChild(option);
+        //get each ship name
             for(element in data.results){
                 let option = createOption(data.results[element].name);
                 shipList.appendChild(option);
@@ -33,3 +39,26 @@ function createOption(text){
     option.textContent = text;
     return option;
 }
+
+// when a spaceship is selected from the list
+shipList.addEventListener("change", function(){
+    console.log(this.value);
+});
+
+// when the button is clicked
+button.addEventListener("click", updateShipData);
+
+// retrieve spaceship data
+async function getShipData(){
+    return fetch(shipUrl).then(response => response.json());
+}
+
+// update spaceship data
+function updateShipData(){
+    getShipData().then(function(data){
+        console.log("Clicked!");
+        //get ship image
+        let image = createOption(data.image_url);
+        console.log(image.innerHTML);
+            })
+        }
