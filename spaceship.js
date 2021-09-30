a89ad13bb1d377bc51939f92b1d6fb76f32ec9d7
Andre <girard176@live.com>
2021-09-29 17:33:04 -0700

    Added shipIndex to shipList

--- spaceship.js
+++ spaceship.js
@@ -4,28 +4,29 @@
     API ref: https://lldev.thespacedevs.com/2.2.0/config/spacecraft/
 */
 
-// variables
-const shipListUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/?in_use=true&human_rated=true&order=name";
+// Variables
+const shipDataUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/?in_use=true&human_rated=true&order=name";
 const shipList = document.getElementById("ship-list");
 const button = document.getElementById("getShip");
-const shipID = 10;
+
+var shipIndex = 0;
 const shipUrl = "https://lldev.thespacedevs.com/2.2.0/config/spacecraft/10/"
 const shipImage = document.getElementById("ship-image")
 
-// when the page load
+// When the page load
 window.addEventListener("load", updateShipList);
 
-// retreive the list of spaceships
-async function getShipList(){
-    return fetch(shipListUrl).then(response => response.json());
+// Retreive the spaceships data
+async function getShipData(){
+    return fetch(shipDataUrl).then(response => response.json());
 }
 
-// add spaceships to drop down list
+// Add spaceships to drop down list
 function updateShipList(){
-    getShipList().then(function(data){
+    getShipData().then(function(data){
             let option = createOption("Select a spaceship");
             shipList.appendChild(option);
-        //get each ship name
+        // Get each ship name
             for(element in data.results){
                 let option = createOption(data.results[element].name);
                 shipList.appendChild(option);
@@ -40,25 +41,27 @@ function createOption(text){
     return option;
 }
 
-// when a spaceship is selected from the list
+// When a spaceship is selected from the list
 shipList.addEventListener("change", function(){
-    console.log(this.value);
+    let ship = this.value;
+    if (ship != "Select a spaceship"){
+        console.log(ship + " selected");
+        shipIndex = this.options[this.selectedIndex].index;
+        console.log(shipIndex)
+    }
 });
 
-// when the button is clicked
-button.addEventListener("click", updateShipData);
+// When the button is clicked
+button.addEventListener("click", function(){
+    console.log("Button clicked")
+    selectShipData();
+});
 
-// retrieve spaceship data
-async function getShipData(){
-    return fetch(shipUrl).then(response => response.json());
-}
-
-// update spaceship data
-function updateShipData(){
+// Select spaceship data
+function selectShipData(){
     getShipData().then(function(data){
-        console.log("Clicked!");
         //get ship image
-        let image = createOption(data.image_url);
+        let image = createOption(data.results[shipIndex-1].image_url);
         console.log(image.innerHTML);
-            })
-        }
+    })
+}
